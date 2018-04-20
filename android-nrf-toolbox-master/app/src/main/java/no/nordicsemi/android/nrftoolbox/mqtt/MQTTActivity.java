647707902c6375368e8d50cb728d50cbb00eb7ad
Duy Tran <lowe.tran@gmail.com>
2018-04-20 21:39:42 +0700

    MQTT GUI added

---
+++ android-nrf-toolbox-master/app/src/main/java/no/nordicsemi/android/nrftoolbox/mqtt/MQTTActivity.java
@@ -0,0 +1,24 @@
+package no.nordicsemi.android.nrftoolbox.mqtt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import no.nordicsemi.android.nrftoolbox.R;
+
+public class MQTTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mqtt);
+    }
+
+    void onServerConnectClicked(final View view) {
+
+    }
+
+    void onUploadClicked(final View view) {
+
+    }
+}
